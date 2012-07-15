15bfd0383b195c0a1e76d2e1ec2509ea5cfce745
lalalainexd <lalalainexd@gmail.com>
2012-07-15 19:05:26 -0400

    generates chapters, pages, and posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :posts
+
+  map.resources :pages
+
+  map.resources :chapters
+
   map.resources :comics
 
   # The priority is based upon order of creation: first created -> highest priority.
